06334a33a60570690f0259df8ada28981b83536f
Anastasia Motolova <motolovanastya@gmail.com>
2021-10-14 21:36:36 -0700

    Terraform-2 updates to backend.tf

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -3,9 +3,9 @@ terraform {
     endpoint   = "storage.yandexcloud.net"
     bucket     = "terr-state-bckt"
     region     = "us-east-1"
-    key        = "stage/terraform.tfstate"
-    access_key = "I9wqmfK8EQL0tCM--fox"
-    secret_key = "IiqXyuZGSvn-DhpkCz3stestyiHhwFVtestHG4-b"
+    key        = "prod/terraform.tfstate"
+    # to pass access/secret keys use command:
+    # terraform init -backend-config="access_key=xxx" -backend-config="secret_key=xxx"
 
     skip_region_validation      = true
     skip_credentials_validation = true
